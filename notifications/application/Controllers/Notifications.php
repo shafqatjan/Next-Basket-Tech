ea827bcc1bce14cd254b13123a733a401a170c71
Shafqat Jan <>
2024-03-30 22:47:02 +0500

    Initial commit

--- notifications/application/Controllers/Notifications.php
+++ notifications/application/Controllers/Notifications.php
@@ -1,14 +1,35 @@
 <?php
-// In application/Controllers/Notifications.php
+// application/Controllers/Notifications.php
 
 namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class Notifications extends ResourceController
 {
-    public function post()
+    public function consumeEvents()
     {
-        // Implement logic to consume events and save data
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('user_events', false, false, false, false);
+
+        echo ' [*] Waiting for user events. To exit press CTRL+C', "\n";
+
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+            // Save the event data to a log file
+            log_message('info', 'Received event: ' . $msg->body);
+        };
+
+        $channel->basic_consume('user_events', '', false, true, false, false, $callback);
+
+        while ($channel->is_consuming()) {
+            $channel->wait();
+        }
+
+        $channel->close();
+        $connection->close();
     }
 }
--- users/application/Controllers/Users.php
+++ users/application/Controllers/Users.php
@@ -1,13 +1,42 @@
 <?php
+// application/Controllers/Users.php
 
 namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class Users extends ResourceController
 {
     public function post()
     {
-        // Implement logic to save user data and publish an event
+        $data = $this->request->getJSON();
+        if ($data) {
+            // Save user data to a database or log file
+            // For simplicity, we'll log the data
+            log_message('info', 'User data: ' . print_r($data, true));
+
+            // Publish an event to RabbitMQ
+            $this->publishEvent($data);
+
+            return $this->respondCreated(['message' => 'User data saved and event published.']);
+        } else {
+            return $this->failValidationError('Invalid data');
+        }
+    }
+
+    private function publishEvent($data)
+    {
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('user_events', false, false, false, false);
+
+        $msg = new AMQPMessage(json_encode($data));
+        $channel->basic_publish($msg, '', 'user_events');
+
+        $channel->close();
+        $connection->close();
     }
 }
